417095eb734f456a0b73eed7c3f99d52b78c23cf
parthee <parthee>
2006-12-15 15:32:10 +0000

    formatting changes

--- src/gov/epa/mims/analysisengine/table/MultiRowTableColumn.java
+++ src/gov/epa/mims/analysisengine/table/MultiRowTableColumn.java
@@ -2,62 +2,31 @@ package gov.epa.mims.analysisengine.table;
 
 import javax.swing.table.*;
 
-/**
- * <p>Description: A table column that contains multiple rows of header
- * information. Used with the MultiRowHeaderRenderer. </p>
- * <p>Copyright: Copyright (c) 2004</p>
- * <p>Company: UNC - CEP</p>
- * @author Daniel Gatti
- * @version $Id: MultiRowTableColumn.java,v 1.2 2005/09/19 14:50:03 rhavaldar Exp $
- */
-public class MultiRowTableColumn extends TableColumn
-{
-   /** The column headers. */
-   protected String[] columnHeaders = null;
+public class MultiRowTableColumn extends TableColumn {
+	protected String[] columnHeaders = null;
 
-   public MultiRowTableColumn(int index, String[] headers)
-   {
-      super(index);
-      columnHeaders  = headers;
-      headerRenderer = new MultiRowHeaderRenderer(headers);
-   } // MultiRowTableColumn()
+	public MultiRowTableColumn(int index, String[] headers) {
+		super(index);
+		columnHeaders = headers;
+		headerRenderer = new MultiRowHeaderRenderer(headers);
+	}
 
+	public Object getHeaderValue() {
+		return columnHeaders;
+	}
 
-   /**
-    * Return the String[] of columnHeaders.
-    * @return String[]
-    */
-   public Object getHeaderValue()
-   {
-      return columnHeaders;
-   } // getHeaderValue()
+	public Object getIdentifier() {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < columnHeaders.length - 1; i++) {
+			sb.append(columnHeaders[i]);
+			sb.append('|');
+		}
+		sb.append(columnHeaders[columnHeaders.length - 1]);
 
+		return sb.toString();
+	}
 
-   /**
-    * Return all of the column headers concatenated with '|' between them.
-    * @return String[]
-    */
-   public Object getIdentifier()
-   {
-      StringBuffer sb = new StringBuffer();
-      for (int i = 0; i < columnHeaders.length - 1; i++)
-      {
-         sb.append(columnHeaders[i]);
-         sb.append('|');
-      }
-      sb.append(columnHeaders[columnHeaders.length - 1]);
-
-      return sb.toString();
-   } // getIdentifier()
-
-
-   /**
-    * Return the special MultiRowHeaderRenderer for this column.
-    * @return MultiRowHeaderRenderer
-    */
-   public TableCellRenderer getTableCellRenderer()
-   {
-      return headerRenderer;
-   } // getTableCellRenderer()
-} //class MultiRowTableColumn
-
+	public TableCellRenderer getTableCellRenderer() {
+		return headerRenderer;
+	}
+}
